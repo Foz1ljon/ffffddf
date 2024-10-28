98f38ba5899b132ca1d417498f942dba919fcbaf
Foz1ljon <faxriddinov0620@gmail.com>
2024-10-28 19:59:56 +0500

    fixed

--- src/components/new-release.vue
+++ src/components/new-release.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="flex justify-between gap-10 h-[240px] !items-center w-full">
-    <div>
-      <img
-        class="w-[413px] bg-cover rounded-2xl"
-        :src="imgSrc"
-        alt="new-release-ionics"
-      />
-    </div>
-    <div class="flex flex-col justify-center gap-2 items-start w-full">
-      <p class="lg:text-[19px] text-base bold tracking-wide text-[#596167]">{{ data?.caption }}</p>
-      <h3 class="text-[30px] bold tracking-wider text-[#020C14]">
+  <div class="flex justify-between gap-10 h-[240px]  items-center w-full">
+    <img class="bg-cover w-[413px] rounded-3xl" :src="imgSrc" alt="new-release-ionics" />
+
+    <div class="flex flex-col gap-5 items-start w-full">
+      <p
+        class="lg:text-[19px] text-base bold tracking-[1.33px] leading-[150%] text-[#596167]"
+      >
+        {{ data?.caption }}
+      </p>
+      <h3 class="text-[30px] bold tracking-[2.1px] text-[#020C14]">
         {{ data?.title }}
       </h3>
       <p
-        class="lg:text-[19px] md:text-base text-sm font-normal tracking-wide text-[#596167]"
+        class="lg:text-[19px] md:text-base text-sm font-normal tracking-[1.33px] leading-[150%] text-[#596167]"
       >
         {{ data?.date }}
       </p>
--- src/views/product-news.vue
+++ src/views/product-news.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="bg-[#edeff6]">
-    <div class="container py-3">
-      <Path :path="t('news')" class="mt-10" />
+    <div class="container w-[1280px] py-3">
+      <Path :path="t('news')" class="mt-[84px] mb-10" />
 
       <h1
         v-if="$i18n.locale == 'uz'"
-        class="text-4xl py-4 bold lg:w-[34%] w-full text-[#ABABB9]"
+        class="text-5xl pb-10 bold lg:w-[44%] tracking-[3.36px] leading-[150%] w-full text-[#ABABB9]"
       >
         <span class="bold text-[#020C14]">Energiyaga</span>
         oid so'nggi yangiliklardan xabardor bo'ling
       </h1>
       <h1
         v-if="$i18n.locale == 'ru'"
-        class="text-4xl py-4 bold lg:w-[34%] w-full text-[#ABABB9]"
+        class="text-5xl pb-10 bold lg:w-[44%] tracking-[3.36px] leading-[150%] w-full text-[#ABABB9]"
       >
         Будьте в курсе последних новостей в сфере
         <span class="bold text-[#020C14]">энергетики</span>
       </h1>
       <h1
         v-if="$i18n.locale == 'eng'"
-        class="text-4xl py-4 bold lg:w-[34%] w-full text-[#ABABB9]"
+        class="text-5xl pb-10 bold lg:w-[44%] tracking-[3.36px] leading-[150%] w-full text-[#ABABB9]"
       >
         Stay up to date with the latest
         <span class="bold text-[#020C14]">energy</span> news
       </h1>
-      <div class="flex justify-evenly">
+
+      <!--  -->
+
+      <div class="flex justify-evenly pb-10">
         <p
           class="text-[30px] text-[#ABABB9] cursor-pointer active:text-[#020C14]"
           v-for="(item, i) in year"
@@ -34,11 +37,11 @@
         </p>
       </div>
 
-      <div class="py-6">
+      <div class="pb-[60px]">
         <Month />
       </div>
 
-      <div v-if="currentPage === 1" class="lg:grid flex flex-wrap gap-2">
+      <div v-if="currentPage === 1" class="lg:grid flex pb-5 flex-wrap gap-5">
         <template v-for="(data, i) in paginatedNews" :key="data.id">
           <NewsCard
             v-if="i < 3"
@@ -49,7 +52,7 @@
         </template>
       </div>
 
-      <div v-if="currentPage === 1" class="grid lg:grid-cols-3 grid-cols-1 gap-3 my-3">
+      <div v-if="currentPage === 1" class="grid lg:grid-cols-3 grid-cols-1 gap-5 pb-5">
         <template v-for="(data, i) in paginatedNews" :key="data.id">
           <NewFuncCard
             v-if="i > 2 && i < 6"
@@ -61,42 +64,48 @@
 
       <div
         v-if="currentPage >= 1"
-        class="grid grid-cols-1 divide-y-2 divide-[#B4BCC1] gap-2"
+        class="grid grid-cols-1 divide-y-2 divide-[#B4BCC1] gap-10"
       >
         <template v-for="(data, i) in paginatedNews" :key="data.id">
           <NewRelease
             v-if="(i > 5 && currentPage === 1) || (i >= 0 && currentPage > 1)"
             @click="$router.push(`/products/${data.id}`)"
             :data="data"
+            class="!pt-10"
           />
         </template>
-      </div>
-
-      <div class="flex justify-center items-center py-4 gap-8">
-        <button
-          class="bg-[#D5DCE9] rotate-180 outline-none p-4 rounded-full active:bg-[#B2B6BE]"
-          @click="prevPage"
-          :disabled="currentPage === 1"
-        >
-          <Arrow />
-        </button>
-        <p class="flex items-center gap-4 text-[#020C14] text-[22px] tracking-wide">
-          <span class="py-2 px-4 rounded-md bg-white border border-gray-400">
-            {{ currentPage }}
-          </span>
-          of {{ totalPages }}
-        </p>
-        <button
-          class="bg-[#D5DCE9] p-4 outline-none rounded-full active:bg-[#B2B6BE]"
-          @click="nextPage"
-          :disabled="currentPage === totalPages"
-        >
-          <Arrow />
-        </button>
+        <div class="flex justify-center items-center gap-8 pt-10 pb-[125px]">
+          <button
+            class="bg-[#D5DCE9] rotate-180 outline-none p-4 rounded-full"
+            @click="prevPage"
+            :class="
+              currentPage == totalPages ? 'active:bg-[#B2B6BE] hover:bg-[#cacdd4]' : ''
+            "
+            :disabled="currentPage === 1"
+          >
+            <Arrow />
+          </button>
+          <p class="flex items-center gap-4 text-[#020C14] text-[22px] tracking-wide">
+            <span class="py-2 px-4 rounded-md bg-white border border-gray-400">
+              {{ currentPage }}
+            </span>
+            of {{ totalPages }}
+          </p>
+          <button
+            class="bg-[#D5DCE9] p-4 outline-none rounded-full"
+            @click="nextPage"
+            :class="
+              currentPage !== totalPages ? 'active:bg-[#B2B6BE]  hover:bg-[#cacdd4]' : ''
+            "
+            :disabled="currentPage === totalPages"
+          >
+            <Arrow />
+          </button>
+        </div>
       </div>
 
       <!-- Tags Section -->
-      <div class="flex gap-4 my-3">
+      <div class="flex gap-4 pb-[60px]">
         <template v-for="(el, i) in tags" :key="i">
           <Tags :data="el" />
         </template>
@@ -309,12 +318,16 @@
       nextPage() {
         if (this.currentPage < this.totalPages) {
           this.currentPage++;
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
         }
       },
 
       prevPage() {
         if (this.currentPage > 1) {
           this.currentPage--;
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
         }
       },
     },
